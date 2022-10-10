8e5583115cb7fadf68fa4344c72fc0d84481b9e7
Dmitriy <potapidze.mos@mail.ru>
2022-10-10 18:47:03 +0300

    Добавил решение задачи №10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -6,3 +6,20 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 999)
+    Console.WriteLine("Вы ввели не трёхзначное число");
+else
+{
+    int firstDigit = number / 100;
+    int twoDigit = number * 10 / 100;
+
+    int twoNumber = twoDigit - firstDigit * 10;
+
+    Console.Write($"Вторая цифра введённого числа: {twoNumber}");
+}
+
+
